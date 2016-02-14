51a0d643fae1f2399149cbda2e39eacae152ebb7
Mahlon Barrault <mahlonbarrault@gmail.com>
2016-02-14 07:05:26 -0700

    Completed adaboost and random forest work for CYOA. Working on lesson 5.

--- lesson4_choose_your_own/your_algorithm.py
+++ lesson4_choose_your_own/your_algorithm.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
-
+import sys
+sys.path.append("../tools/")
+from time import time
 import matplotlib.pyplot as plt
 from prep_terrain_data import makeTerrainData
 from class_vis import prettyPicture
@@ -31,14 +33,84 @@ plt.show()
 ### your code here!  name your classifier object clf if you want the 
 ### visualization code (prettyPicture) to show you the decision boundary
 
+# Try AdaBoost
 
+def adaboost(n_estimators, learning_rate):
+    
+    from sklearn.ensemble import AdaBoostClassifier
+    from sklearn.metrics import accuracy_score
+    
+    clf = AdaBoostClassifier(n_estimators = n_estimators, 
+                             learning_rate = learning_rate)
+    clf.fit(features_train, labels_train)
+    
+    t_fit = time()
+    clf.fit(features_train, labels_train)
+    print "training time:", round(time()-t_fit, 3), "s"
+    
+    t_pred = time()
+    pred = clf.predict(features_test)
+    print "predict time:", round(time()-t_pred, 3), "s"
+    
+    print accuracy_score(pred, labels_test)
 
+    try:
+        prettyPicture(clf, features_test, labels_test)
+    except NameError:
+        pass
 
+# Try Random Forest
 
+def ranforest(n_estimators, min_samples_split):
+    
+    from sklearn.ensemble import RandomForestClassifier
+    from sklearn.metrics import accuracy_score
+    
+    clf = RandomForestClassifier(n_estimators = n_estimators, 
+                                 min_samples_split = min_samples_split,
+                                 bootstrap = True)
+    clf.fit(features_train, labels_train)
+    
+    t_fit = time()
+    clf.fit(features_train, labels_train)
+    print "training time:", round(time()-t_fit, 3), "s"
+    
+    t_pred = time()
+    pred = clf.predict(features_test)
+    print "predict time:", round(time()-t_pred, 3), "s"
+    
+    print accuracy_score(pred, labels_test)
 
+    
+    try:
+        prettyPicture(clf, features_test, labels_test)
+    except NameError:
+        pass
 
-
-try:
-    prettyPicture(clf, features_test, labels_test)
-except NameError:
-    pass
+def supportvector(C, gamma = 'default'):
+    
+    from sklearn.svm import SVC
+    from sklearn.metrics import accuracy_score
+    
+    if gamma == 'default':
+        clf = SVC(kernel="rbf", C = C)
+    else:
+        clf = SVC(kernel="rbf", C = C, gamma = gamma)
+    
+    clf.fit(features_train, labels_train)
+    
+    t_fit = time()
+    clf.fit(features_train, labels_train)
+    print "training time:", round(time()-t_fit, 3), "s"
+    
+    t_pred = time()
+    pred = clf.predict(features_test)
+    print "predict time:", round(time()-t_pred, 3), "s"
+    
+    print accuracy_score(pred, labels_test)
+    
+    try:
+        prettyPicture(clf, features_test, labels_test)
+    except NameError:
+        pass
+    
--- lesson5_datasets_questions/explore_enron_data.py
+++ lesson5_datasets_questions/explore_enron_data.py
@@ -17,6 +17,30 @@
 
 import pickle
 
-enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
+enron_data = pickle.load(open("../lesson15_final_project/final_project_dataset.pkl", "r"))
 
+enron_data[enron_data.keys()[0]].keys()
 
+enron_keys = enron_data.keys()
+enron_keys.sort()
+
+# How many people are there in the dataset
+len(enron_data)
+
+# How many features per person
+len(enron_data["SKILLING JEFFREY K"])
+
+# How many POIs
+poi = [enron_data[ii]["poi"] for ii in enron_data.keys() if enron_data[ii]["poi"] == 1]
+len(poi)
+
+# How many POI total
+f = open('../lesson15_final_project/poi_names.txt', 'r')
+x = f.readlines()
+print len(x) - 2 # Account for the two lines at the start of the file
+
+# What is the total value of the stock belonging to James Prentice?
+enron_data["PRENTICE JAMES"]['total_stock_value']
+
+# How many email messages do we have from Wesley Colwell to persons of interest?
+enron_data["COLWELL WESLEY"]['from_this_person_to_poi']
