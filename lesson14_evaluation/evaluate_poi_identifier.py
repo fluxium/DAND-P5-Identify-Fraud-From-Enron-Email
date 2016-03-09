91d2f7f9dd3ac1006997f8d33a415c90fd4a7433
Mahlon Barrault <mahlonbarrault@gmail.com>
2016-03-09 08:30:16 -0700

    Adding final work from lesson 14. Adding print statements for precision and recall scores.

--- lesson14_evaluation/evaluate_poi_identifier.py
+++ lesson14_evaluation/evaluate_poi_identifier.py
@@ -16,7 +16,7 @@ import sys
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
-data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
+data_dict = pickle.load(open("../lesson15_final_project/final_project_dataset.pkl", "r") )
 
 ### add more features to features_list!
 features_list = ["poi", "salary"]
@@ -27,5 +27,25 @@ labels, features = targetFeatureSplit(data)
 
 
 ### your code goes here 
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn import cross_validation
 
+features_train, features_test, labels_train, labels_test = \
+cross_validation.train_test_split(features, 
+                                  labels, 
+                                  test_size = 0.3, 
+                                  random_state = 42)
 
+clf = DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+print accuracy_score(labels_test, pred)
+print precision_score(labels_test, pred)
+print recall_score(labels_test, pred)
+
+for i in range(0, len(pred)):
+    if pred[i] == 1.0 and pred[i] == labels_test[i]:
+        print "True Positive" + str(i)
